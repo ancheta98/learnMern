03a3be72eeda1430064a79a20ce21053fb46c1e2
Angel Ancheta <angelancheta98@yahoo.com>
2018-09-13 13:10:31 -0700

    starting back up

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const express = require('express');
+const express = require("express");
+require("./services/passport");
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({bye: 'buddy'})
-});
+//requiring authroutes returns a function and is executed immediately bc of the second set
+//of parentheses
+require("./routes/authRoutes")(app);
 
-//dynamically finds port to use 
-const PORT = process.env.PORT || 5000
+//dynamically finds port to use
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
