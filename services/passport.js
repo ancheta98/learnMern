196f6cad5e4956a98e370c66f18af0d8c39af71e
Angel Ancheta <angelancheta98@yahoo.com>
2018-09-30 22:33:43 -0700

    new stuff

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,9 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
 const keys = require("../config/keys");
+
+const User = mongoose.model("users");
 
 passport.use(
   new GoogleStrategy(
@@ -9,10 +12,20 @@ passport.use(
       clientSecret: keys.googleClientSecret,
       callbackURL: "/auth/google/callback"
     },
-    (accessToken, refreshToken, profile) => {
-      console.log("access token", accessToken);
-      console.log("refresh token", refreshToken);
-      console.log("profile", profile);
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({ googleId: profile.id })
+        .then((existingUser) => {
+          if(existingUser){
+            //we already have a record w/ given profile id
+            done(null,existingUser);
+          } else{
+            //we don't have, make a new record
+            new User({ gooogleId: profile.id })
+              .save()
+              .then(user => done(null, user))
+          }
+        })
+
     }
   )
 );
