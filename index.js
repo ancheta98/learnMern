0f91a1f82b9d5728fb9a26b155a5dcc14ba2cbef
Angel Ancheta <angelancheta98@yahoo.com>
2018-10-02 19:38:23 -0700

    updated passport and cookies

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
 const keys = require("./config/keys");
 require("./models/User");
 require("./services/passport");
@@ -7,6 +9,15 @@ require("./services/passport");
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey] //in an array bc it can choose multiple keys
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 //requiring authroutes returns a function and is executed immediately bc of the second set
 //of parentheses
