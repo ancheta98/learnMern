ffddee41df4ce93f1b6f4814be9932594733b16c
Angel Ancheta <angelancheta98@yahoo.com>
2018-10-17 13:28:38 -0700

    implemented es2017 syntax for passport

--- services/passport.js
+++ services/passport.js
@@ -5,14 +5,13 @@ const keys = require("../config/keys");
 
 const User = mongoose.model("users");
 passport.serializeUser((user, done) => {
-  done(null, user.id)
+  done(null, user.id);
 });
 
 passport.deserializeUser((id, done) => {
-  User.findById(id)
-    .then(user => {
-      done(null, user);
-    });
+  User.findById(id).then(user => {
+    done(null, user);
+  });
 });
 
 passport.use(
@@ -23,20 +22,15 @@ passport.use(
       callbackURL: "/auth/google/callback",
       proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id })
-        .then((existingUser) => {
-          if(existingUser){
-            //we already have a record w/ given profile id
-            done(null,existingUser);
-          } else{
-            //we don't have, make a new record
-            new User({ gooogleId: profile.id })
-              .save()
-              .then(user => done(null, user))
-          }
-        })
-
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+      if (existingUser) {
+        return done(null, existingUser);
+      }
+        const user = await new User({ gooogleId: profile.id }).save();
+        done(null, user);
     }
   )
 );
+
+//notes: everytime we touch the database, we return a promise
